678ca03b6e4ff9f25323a0bc8670c8d8c7985312
dul <novaline@qq.com>
2015-07-29 21:37:29 +0800

    test

--- app/rollerPicker/controller.js
+++ app/rollerPicker/controller.js
@@ -4,4 +4,12 @@
 myAppControllers
     .controller('FController', ['$scope', '$log', function ($scope, $log) {
 
+        $scope.config = {
+            name: 'novaline',
+            email: 'novaline@qq.com'
+        };
+
+        $scope.vm = {
+            money: 8888
+        }
     }]);
--- app/rollerPicker/directive.js
+++ app/rollerPicker/directive.js
@@ -3,13 +3,24 @@
  */
 myAppDirectives
     .constant('rollerPickerConfig', {})
-    .directive('rollerPicker', ['$document', function ($document) {
+    .directive('rollerPicker', ['$document','$timeout','$parse',  function ($document, $timeout, $parse) {
         return {
             restrict: 'EA',
             replace: true,
-            require: 'ngModel',
-            link: function (scope, element, attrs, NgModelController) {
-                debugger;
+            templateUrl: './templates/directive/rollerPicker.tpl.html',
+            link: function (scope, element, attrs) {
+                var isBool = scope.$eval(attrs.isBool),
+                    isNumber = scope.$eval(attrs.isNum),
+                    config = scope.$eval(attrs.config);
+
+
+                var money = $parse(attrs.money);
+
+                $timeout(function() {
+                    money.assign(scope, 9999);
+                }, 2000);
+
+
             }
         }
     }]);
